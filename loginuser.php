d0f69b95cafa36515749e1a78488988affecd12d
Reza Setiawan <rezasetiawan1220@gmail.com>
2023-07-01 16:52:32 +0700

    Mengubah Judul

--- loginuser.php
+++ loginuser.php
@@ -18,7 +18,7 @@ if (!empty($_SESSION['status'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>LOGIN</title>
   <link rel="stylesheet" href="output.css">
 </head>
 
